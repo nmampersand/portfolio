699a4dd50afee3a09ff55b73d983e1a9e38b64aa
Nicole Ampersand <nicoleampersand@Nicoles-MacBook-Pro.local>
2020-07-11 17:38:24 -0700

    base layout v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import avatar from './avatar.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="main">
+        <img className="avatar" src={avatar} alt="pixelated avatar image of Nicole" />
+        <h2>hi</h2>
+        </div>
+        <div className="nav">
+          <div className="navText">about</div>
+          <div className="navText">projects</div>
+          <div className="navText">resume</div>
+        </div>
+      </div>
     </div>
   );
 }
