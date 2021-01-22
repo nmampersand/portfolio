7b2c33a76a4f79cb414b1df461c93caafde05d69
Nicole Ampersand <nicoleampersand@Nicoles-MacBook-Pro.local>
2021-01-22 14:05:02 -0800

    update copyright date and hide routes not being used yet

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -28,13 +28,6 @@ const About = () => (
       >
         <i className="fab fa-github"></i>
       </a>
-      <a 
-        href='https://www.instagram.com/nicoleampersand/'
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fab fa-instagram"></i>
-      </a>
       <a
         href='https://dev.to/nmampersand'
         target="_blank"
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,24 +2,26 @@ import React from 'react';
 import {Router} from '@reach/router';
 import About from '../About';
 import './index.css';
-import Nav from '../Nav';
-import Blog from '../Blog';
-import Projects from '../Projects';
+// import Nav from '../Nav';
+// import Blog from '../Blog';
+// import Projects from '../Projects';
 import NotFound from '../NotFound';
 
 const App = () => {
   return (
     <div className="app">
-      <Nav />
+      {/* uncomment when other routes are ready */}
+      {/* <Nav /> */}
       <div className='main'>
         <Router>
           <About path='/' />
-          <Blog path='/blog' />
-          <Projects path='/projects' />
+          {/* uncomment when routes are ready */}
+          {/* <Blog path='/blog' />
+          <Projects path='/projects' /> */}
           <NotFound default />
         </Router>
       </div>
-      <p className='copyright'>© 2020 Nicole Ampersand</p>
+      <p className='copyright'>© { new Date().getFullYear() } Nicole Ampersand</p>
     </div>
   );
 };
